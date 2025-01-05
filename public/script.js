77e31a22608b5a687ad4f51ad54466b737922ded
SamuelhaIIman <144782909+SamuelhaIIman@users.noreply.github.com>
2025-01-05 22:32:01 +0200

    shop-logic-built-by-me

--- public/script.js
+++ public/script.js
@@ -1,13 +1,41 @@
+// const cart = {};
+// let totalItems = 0;
+
+// function updateCart(productId, change) {
+    
+//     if (!cart[productId]) cart[productId] = 0;
+
+//     cart[productId] = Math.max(cart[productId] + change, 0);
+
+//     totalItems = Object.values(cart).reduce((sum, qty) => sum + qty, 0);
+    
+//     document.getElementById(`Quantity-${productId}`).textContent = cart[productId];
+// }
+
+// function addToCart(productId) {
+//     document.getElementById(`Quantity-${productId}`).textContent = 0;
+//     cart[productId] = 0;
+// }
+
 const cart = {};
-let totalItems = 0;
 
 function updateCart(productId, change) {
-    
-    if (!cart[productId]) cart[productId] = 0;
+    // I create the object
+    if(!cart[productId]) {
+        cart[productId] = 0;
+    }
+    // I add here to the products amount
+    if(cart.hasOwnProperty(productId)) {
+        cart[productId] += change;
+    }
+    // I check the produts amount that it doesn't go below zero
+    if(cart[productId] < 0) {
+        cart[productId] = 0;
+    }else {
+        document.getElementById(`Quantity-${productId}`).textContent = cart[productId];
+    }
+}
 
-    cart[productId] = Math.max(cart[productId] + change, 0);
-
-    totalItems = Object.values(cart).reduce((sum, qty) => sum + qty, 0);
-    
-    document.getElementById(`Shop-Front-Product-Amount-${productId}`).textContent = cart[productId];
+function addToCart(productId) {
+    //document.getElementById(`Quantity-${productId}`).textContent = cart[productId];
 }
